5710b698ef659e830a7de48a7488076cdb64c840
Nazhim <nazhimkalam@gmail.com>
2021-05-29 20:11:56 +0530

    completed about header and footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -11,6 +11,13 @@ const HeaderSC = styled.div`
 	h1 {
 		color: #5465ff;
 	}
+	margin: 00px 20px 20px 20px;
+	padding: 20px;
+	width: 100%;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	/* border-bottom: 1px #BFD7FF solid; */
 `;
 
 export default Header;
